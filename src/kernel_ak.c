2b4719d45c6517b4ec28e5b302936f6acfd3f003
Josiah Welch <josiahwelch@jw-thinkpad.my.domain>
2025-08-29 14:52:39 -0500

    added gitignore and kernel c file

---
+++ src/kernel_ak.c
@@ -0,0 +1,44 @@
+// Josiah Welch
+// 8/29/25
+// Kernel c file
+
+//NOTE: heavily influenced by https://github.com/chipsetx/Simple-Kernel-in-C-and-Assembly#
+
+#define COLOR_SCHEME 0x07 // light gray bg with black fg
+
+int print_ak(char *msg, unsigned int line) {
+	char *mem = (char *) 0xb8000;
+	unsigned int i = (line * 80 * 2);
+
+	while (*msg != 0) {
+		if (*msg == '\n') {
+			line++; // Since newline goes to the next line...
+			i = (line * 80 * 2);
+			*msg++;
+		} else {
+			mem = (char *) 0xb8000;
+			*msg++;
+			i++;
+			mem[i] = COLOR_SCHEME;
+			i++;
+		};
+	};
+
+	return 1;
+}
+
+void clear_ak() {
+	char *mem = (char *) 0xb8000;
+	unsigned int i = 0;
+	while (i < (80 * 2 * 25)) {
+		mem[i] = ' ';
+		i++;
+		mem[i] = COLOR_SCHEME;
+		i++;
+	};
+};
+
+void main_al() {
+	clear_ak();
+	print_ak("Arca kernel loaded!\nWelcome to Arca OS!", 0);
+};

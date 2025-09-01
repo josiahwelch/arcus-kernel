a1740f206f3e16872b4a67952ae39c284d8d6c36
Josiah Welch <josiah.isaiah.welch@gmail.com>
2025-08-31 22:58:00 -0500

    added IDT_MAX_DESCRIPTORS

--- src/kernel_ak.c
+++ src/kernel_ak.c
@@ -16,11 +16,16 @@ void exception_handler() {
 	    __asm__ volatile ("cli; hlt"); // Completely hangs the computer
 }
 
+static int vectors[IDT_MAX_DESCRIPTORS];
+
+extern void* isr_stub_table[];
+
 // Creates an IDT table
 __attribute__((aligned(0x10))) 
 static idt_entry_t idt[256]; // Create an array of IDT entries; aligned for performance
 static idtr_t idtr;
 
+// Helper function for entry definition
 void idt_set_descriptor(unsigned char vector, void* isr, unsigned char flags);
 void idt_set_descriptor(unsigned char vector, void* isr, unsigned char flags) {
 	idt_entry_t* descriptor = &idt[vector];
@@ -30,6 +35,20 @@ void idt_set_descriptor(unsigned char vector, void* isr, unsigned char flags) {
 	descriptor->attributes     = flags;
 	descriptor->isr_high       = (unsigned int)isr >> 16;
 	descriptor->reserved       = 0;
+}
+
+void idt_init(void);
+void idt_init() {
+	idtr.base = (unsigned int *)&idt[0];
+	idtr.limit = (unsigned short)sizeof(idt_entry_t) * IDT_MAX_DESCRIPTORS - 1;
+
+	for (unsigned char vector = 0; vector < 32; vector++) {
+		idt_set_descriptor(vector, isr_stub_table[vector], 0x8E);
+		vectors[vector] = 1;
+	}
+
+	__asm__ volatile ("lidt %0" : : "m"(idtr)); // load the new IDT
+	__asm__ volatile ("sti"); // set the interrupt flag
 }
 
 void main_ak() {

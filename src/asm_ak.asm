0ded177dcc2712399508f7f129092e641f7d5853
Josiah Welch <josiah.isaiah.welch@gmail.com>
2025-09-05 22:22:04 -0500

    added macros to assembly file and added helper function

--- src/asm_ak.asm
+++ src/asm_ak.asm
@@ -2,7 +2,74 @@
 ; 8/29/25
 ; Assembly code to boot Arcus kernel from Grub or other bootloader
 
-; NOTE: heavily influenced by https://github.com/chipsetx/Simple-Kernel-in-C-and-Assembly#
+%macro isr_err_stub 1
+isr_stub_%+%1:
+    call exception_handler
+	iret 
+%endmacro
+
+; if writing for 64-bit, use iretq instead
+%macro isr_no_err_stub 1
+isr_stub_%+%1:
+	call exception_handler
+	iret
+%endmacro
+
+// Using these two macros...
+extern exception_handler
+isr_no_err_stub 0
+isr_no_err_stub 1
+isr_no_err_stub 2
+isr_no_err_stub 3
+isr_no_err_stub 4
+isr_no_err_stub 5
+isr_no_err_stub 6
+isr_no_err_stub 7
+isr_err_stub    8
+isr_no_err_stub 9
+isr_err_stub    10
+isr_err_stub    11
+isr_err_stub    12
+isr_err_stub    13
+isr_err_stub    14
+isr_no_err_stub 15
+isr_no_err_stub 16
+isr_err_stub    17
+isr_no_err_stub 18
+isr_no_err_stub 19
+isr_no_err_stub 20
+isr_no_err_stub 21
+isr_no_err_stub 22
+isr_no_err_stub 23
+isr_no_err_stub 24
+isr_no_err_stub 25
+isr_no_err_stub 26
+isr_no_err_stub 27
+isr_no_err_stub 28
+isr_no_err_stub 29
+isr_err_stub    30
+isr_no_err_stub 31
+
+// Stub table
+global isr_stub_table
+isr_stub_table:
+%assign i 0 
+%rep    32 
+    dd isr_stub_%+i ; use DQ instead if targeting 64-bit
+%assign i i+1 
+%endrep
+
+// Helper function
+void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags);
+void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags) {
+	idt_entry_t* descriptor = &idt[vector];
+
+	descriptor->isr_low        = (uint32_t)isr & 0xFFFF;
+	descriptor->kernel_cs      = 0x08; // this value can be whatever offset your kernel code selector is in your GDT
+	descriptor->attributes     = flags;
+	descriptor->isr_high       = (uint32_t)isr >> 16;
+	descriptor->reserved       = 0;
+}
 
 bits 32
 section .text
--- src/idt.c
+++ src/idt.c
@@ -17,6 +17,17 @@ void exception_handler() {
 	__asm__ volatile ("cli; hlt"); // Completely hangs the computer
 }
 
+void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags);
+void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags) {
+	    idt_entry_t* descriptor = &idt[vector];
+
+		    descriptor->isr_low        = (uint32_t)isr & 0xFFFF;
+			    descriptor->kernel_cs      = 0x08; // this value can be whatever offset your kernel code selector is in your GDT
+				    descriptor->attributes     = flags;
+					    descriptor->isr_high       = (uint32_t)isr >> 16;
+						    descriptor->reserved       = 0;
+}
+
 // Creates an IDT table
 __attribute__((aligned(0x10))) 
 static idt_entry_t idt[256]; // Create an array of IDT entries; aligned for performance

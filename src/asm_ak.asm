b242921e8611a463123fbdf864e244ba6eacc4d1
Josiah Welch <josiah.isaiah.welch@gmail.com>
2025-09-30 12:48:51 -0500

    added lidt to assembly file

--- src/asm_ak.asm
+++ src/asm_ak.asm
@@ -3,6 +3,12 @@
 ; Assembly code to boot Arcus kernel from Grub or other bootloader
 
 bits 32
+global lidt
+lidt:
+    mov eax, [esp+4]
+    lidt [eax]
+    ret
+
 section .text
 	align 4
 	dd 0x1badb002			; Magic #

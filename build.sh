f5d76a3b62aebf7209bb96ab3054589670f874e6
Josiah Welch <josiah.isaiah.welch@gmail.com>
2025-11-04 15:40:34 -0600

    added POSIX complaince to build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir build
-pushd build/
+cd build/
 nasm -f elf32 ../src/asm_ak.asm -o asm_ak.o
 gcc -m32 -ffreestanding -fno-stack-protector -fno-pic -fno-omit-frame-pointer -c ../src/kernel_ak.c -o kernel_ak.o
 #ld -m elf_i386 -T ../src/linker.ld -o kernel asm_ak.o kernel_ak.o
@@ -9,4 +9,4 @@ gcc -m32 -ffreestanding -nostdlib \
     -o kernel \
     kernel_ak.o asm_ak.o\
     -lgcc
-popd
+cd ../
